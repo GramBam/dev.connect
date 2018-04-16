388fde8028bf91c4c793af60fbd2bff5108a277f
GramBam <graham.moss@hotmail.com>
2018-04-16 18:07:15 -0400

    setup email/password login

--- routes/api/users.js
+++ routes/api/users.js
@@ -40,4 +40,29 @@ router.post('/register', (req, res) => {
   });
 });
 
+// @route  GET api/users/login
+// @desc   Login User / Returning Json Web Token (JWT)
+// @access Public
+router.post('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(user => {
+    //Check for user
+    if (!user) {
+      return res.status(404).json({ email: 'User not found' });
+    }
+
+    // Check Password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        res.json({ message: 'Success' });
+      } else {
+        return res.status(400).json({ password: 'Password incorrect' });
+      }
+    });
+  });
+});
+
 module.exports = router;

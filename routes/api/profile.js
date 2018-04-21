692bffdff2d6803c228a13da94423cea64050491
GramBam <graham.moss@hotmail.com>
2018-04-21 16:56:55 -0400

    more profile api routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -38,8 +38,64 @@ router.get(
   }
 );
 
+// @route  GET api/profile/all
+// @desc   Get all Profiles
+// @access Public
+router.get('/all', (req, res) => {
+  const errors = {};
+
+  Profile.find()
+    .populate('user', ['name', 'avatar'])
+    .then(profiles => {
+      if (!profiles) {
+        errors.noprofile = 'No Profiles Exist';
+        return res.status(404).json();
+      }
+      res.json(profiles);
+    })
+    .catch(err => res.status(404).json({ profile: 'There are no profiles' }));
+});
+
+// @route  GET api/profile/handle/:handle
+// @desc   Get Profile by Handle
+// @access Public
+
+router.get('/handle/:handle', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.handle })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
+// @route  GET api/profile/user/:user_id
+// @desc   Get Profile by User ID
+// @access Public
+
+router.get('/user/:user_id', (req, res) => {
+  const errors = {};
+
+  Profile.findOne({ handle: req.params.user_id })
+    .populate('user', ['name', 'avatar'])
+    .then(profile => {
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        res.status(404).json(errors);
+      }
+      res.json(profile);
+    })
+    .catch(err => res.status(404).json(err));
+});
+
 // @route  POST api/profile
-// @desc   Create User Profile
+// @desc   Create or Edit User Profile
 // @access Private
 router.post(
   '/',
@@ -50,7 +106,9 @@ router.post(
     // Check Validation
     if (!isValid) {
       // Reuturn any errors with 400 status
-      return res.status(400).json(errors);
+      return res
+        .status(400)
+        .json({ profile: 'There is no profile for this user' });
     }
 
     // Get fields
